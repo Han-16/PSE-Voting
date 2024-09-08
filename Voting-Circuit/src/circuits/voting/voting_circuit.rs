e07e8e0efa52d7d65535da129c6d41ef07b125bb
Han-16 <haanbk16@gmail.com>
2024-09-08 18:53:22 +0900

    feat: add Merkle tree verification in Voting circuit

--- Voting-Circuit/src/circuits/voting/voting_circuit.rs
+++ Voting-Circuit/src/circuits/voting/voting_circuit.rs
@@ -3,9 +3,13 @@ use ark_ec::CurveGroup;
 use ark_ff::{Field, PrimeField};
 use ark_r1cs_std::{prelude::*, fields::fp::FpVar};
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
-use ark_crypto_primitives::{crh::{poseidon::constraints::{CRHGadget, CRHParametersVar, TwoToOneCRHGadget}, CRHSchemeGadget, TwoToOneCRHSchemeGadget}, sponge::{poseidon::PoseidonConfig, Absorb}};
+use ark_crypto_primitives::{
+    crh::{poseidon::constraints::{CRHGadget, CRHParametersVar}, CRHSchemeGadget}, 
+    sponge::{poseidon::PoseidonConfig, Absorb},
+    merkle_tree::{self, constraints::PathVar},
+};
 
-use super::MockingCircuit;
+use crate::circuits::voting::merkle_tree::{MerkleTreeParams, MerkleTreeParamsVar};
 
 pub type ConstraintF<C> = <<C as CurveGroup>::BaseField as Field>::BasePrimeField;
 
@@ -27,8 +31,8 @@ where
     pub vote_m: Option<Vec<C::BaseField>>,
     pub vote_r: Option<Vec<C::BaseField>>,
     pub sn: Option<C::BaseField>,
-    // pub leaf_pos: Option<u32>,
-    // pub tree_proof: ..?
+    pub leaf_pos: Option<u32>,
+    pub tree_proof: Option<merkle_tree::Path<MerkleTreeParams<C::BaseField>>>,
 }
 
 
@@ -42,7 +46,7 @@ where
 {
     pub g: C::Affine,
     pub ck: Vec<C::Affine>,
-    pub params: PoseidonConfig<C::BaseField>,
+    pub hash_params: PoseidonConfig<C::BaseField>,
     pub instance: VotingInstance<C>,
     pub witness: VotingWitness<C>,
     _curve: PhantomData<GG>,
@@ -58,7 +62,7 @@ where
     fn generate_constraints(self, cs: ConstraintSystemRef<C::BaseField>) -> ark_relations::r1cs::Result<()> {
         let g = GG::new_constant(cs.clone(), self.g)?;
         let ck = Vec::<GG>::new_constant(cs.clone(), self.ck)?;
-        let params = CRHParametersVar::<C::BaseField>::new_constant(cs.clone(), self.params)?;
+        let hash_params = CRHParametersVar::<C::BaseField>::new_constant(cs.clone(), self.hash_params)?;
         let zero = FpVar::<C::BaseField>::zero();
         let one = FpVar::<C::BaseField>::one();
 
@@ -75,6 +79,12 @@ where
         let vote_m = Vec::<FpVar<C::BaseField>>::new_witness(cs.clone(), || self.witness.vote_m.ok_or(SynthesisError::AssignmentMissing))?;
         let vote_r = Vec::<FpVar<C::BaseField>>::new_witness(cs.clone(), || self.witness.vote_r.ok_or(SynthesisError::AssignmentMissing))?;
         let sn = FpVar::<C::BaseField>::new_witness(cs.clone(), || self.witness.sn.ok_or(SynthesisError::AssignmentMissing))?;
+        let leaf_pos = UInt32::new_witness(cs.clone(), || self.witness.leaf_pos.ok_or(SynthesisError::AssignmentMissing))?.to_bits_le();
+        let mut cw = PathVar::<
+                MerkleTreeParams<C::BaseField>,
+                C::BaseField,
+                MerkleTreeParamsVar<C::BaseField>,
+            >::new_witness(cs.clone(), || self.witness.tree_proof.ok_or(SynthesisError::AssignmentMissing))?;
 
 
         // Constraints
@@ -91,13 +101,13 @@ where
         let pk_y = Boolean::le_bits_to_fp_var(&pk_bits[pk_bits.len() / 2..])?;
 
         let hash_input = vec![pk_x, pk_y];
-        let addr_computed = CRHGadget::<C::BaseField>::evaluate(&params, &hash_input)?;
+        let addr_computed = CRHGadget::<C::BaseField>::evaluate(&hash_params, &hash_input)?;
         addr_computed.enforce_equal(&addr)?;
 
 
         // 3. Check sn = CRH(voting_round || sk)
         let hash_input = vec![voting_round, sk];
-        let sn_computed = CRHGadget::<C::BaseField>::evaluate(&params, &hash_input)?;
+        let sn_computed = CRHGadget::<C::BaseField>::evaluate(&hash_params, &hash_input)?;
         sn_computed.enforce_equal(&sn)?;
 
         
@@ -122,6 +132,10 @@ where
         
 
         // 7. MT.verify(addr, path, root) = true
+        let leaf_g = vec![addr.clone()];
+        cw.set_leaf_position(leaf_pos.clone());
+        let path_check = cw.verify_membership(&hash_params, &hash_params, &root, &leaf_g)?;
+        path_check.enforce_equal(&Boolean::Constant(true))?;
         Ok(())
     }
 }
