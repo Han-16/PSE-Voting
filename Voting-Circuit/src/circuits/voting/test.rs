36edfc7c4d8e5634bd5aa39df2cbaf358a86a747
Han-16 <haanbk16@gmail.com>
2024-09-19 02:18:53 +0900

    refactor(test): move test from setup to test.rs

--- Voting-Circuit/src/circuits/voting/test.rs
+++ Voting-Circuit/src/circuits/voting/test.rs
@@ -1,62 +1,36 @@
 #[cfg(test)]
 mod test {
+    use ark_bn254::Bn254;
+    use ark_ec::AffineRepr;
     use crate::circuits::voting::{voting_circuit::VotingCircuit, MockingCircuit};
-    use ark_crypto_primitives::{
-        crh::{poseidon::{TwoToOneCRH, CRH}, CRHScheme, TwoToOneCRHScheme},
-        merkle_tree::{self, constraints::PathVar, MerkleTree},
-        sponge::{poseidon::PoseidonConfig, Absorb}
-    };
-    use ark_ec::{CurveGroup, AffineRepr};
     use ark_relations::r1cs::ConstraintSystem;
-    use std::str::FromStr;
-    use crate::circuits::voting::parser::{User, ParsedUser,get_users, get_ck, get_g, get_user, parse_user};
+    use ark_groth16::Groth16;
+    use ark_crypto_primitives::snark::SNARK;
+    use crate::circuits::voting::parser::{get_ck, get_g, get_user, parse_user};
+    use crate::circuits::voting::{
+        setup::voting_setup,
+        prover::voting_prove
+    };
     
     type C = ark_ed_on_bn254::EdwardsProjective;
     type GG = ark_ed_on_bn254::constraints::EdwardsVar;
 
     type F = ark_bn254::Fr;
 
-    #[test]
-    fn test_poseidon_hash() {
-        use crate::circuits::voting::poseidon_params::get_poseidon_params;
 
-        let hash_params: PoseidonConfig<<<C as CurveGroup>::Affine as AffineRepr>::BaseField> = get_poseidon_params();
-
-        
-        let left_1 = F::from_str("5606718420606443971671484895049764524137380043712288683633077955014799326746").unwrap();
-        let right_1 = F::from_str("17197717161227025415653362559402002071764119382426881957576028977161869254459").unwrap();
-        
-        let left_2 = F::from_str("1183541264794662596260109398285142046445627481274933684025919000314869820549").unwrap();
-        let right_2 = F::from_str("2798884284280419056895756479481872730400655255609110844083423702997077305745").unwrap();
-        
-        let result_1_eval = CRH::<F>::evaluate(&hash_params, vec![left_1, right_1]).unwrap();
-        let result_2_eval = CRH::<F>::evaluate(&hash_params, vec![left_2, right_2]).unwrap();
-        let result_1_comp = TwoToOneCRH::<F>::compress(&hash_params, left_1, right_1).unwrap();
-        let result_2_comp = TwoToOneCRH::<F>::compress(&hash_params, left_2, right_2).unwrap();
-        
-        let expected_root = F::from_str("7589531496280468984030793369747668473312938794789330702447487778151327843690").unwrap();
-        let root_eval = CRH::<F>::evaluate(&hash_params, vec![result_1_eval, result_2_eval]).unwrap();
-        let root_comp = TwoToOneCRH::<F>::compress(&hash_params, result_1_comp, result_2_comp).unwrap();
-
-        assert_eq!(root_eval, expected_root);
-        assert_eq!(root_comp, expected_root);
-    }
-
-    #[test]
-    fn test_voting_constraints() {
-        use ark_relations::r1cs::ConstraintSynthesizer;
-        let tree_height = 4;
-        let voting_round = 1;
-        let num_of_candidates = 2;
-        let num_of_voters = 12;
-        let vote_index = 1;
-        let voter_pos = 10;
-        let candidate_limit = 10;
+    fn make_mocking_circuit() -> VotingCircuit<C, GG> {
+        let tree_height = 10;       // constant
+        let voting_round = 2;       // mutable
+        let num_of_candidates = 10; // mutable
+        let num_of_voters = 3;     // mutable
+        let vote_index = 2;         // mutable
+        let voter_pos = 0;          // mutable (Mock data에 addr이 0 ~ 9)까지 준비되어있음. num_of_voters보다 작아야함
+        let candidate_limit = 10;   // constant
         
         
         let g = get_g().unwrap();
         let ck = get_ck().unwrap();
-        let user = get_user(0).unwrap();
+        let user = get_user(voter_pos.clone() as usize).unwrap();
         let parsed_user = parse_user(&user).unwrap();
         let sk = parsed_user.sk;
         let pk = parsed_user.pk;
@@ -65,10 +39,64 @@ mod test {
             g, ck, sk, pk, tree_height, voting_round, num_of_candidates, num_of_voters, vote_index, voter_pos, candidate_limit
         ).unwrap();
 
+        test_circuit
+    }
+
+    #[test]
+    fn test_voting_constraints() {
+        use ark_relations::r1cs::ConstraintSynthesizer;
+        let test_circuit = make_mocking_circuit();
         let cs = ConstraintSystem::<F>::new_ref();
 
         test_circuit.clone().generate_constraints(cs.clone()).unwrap();
         println!("Number of constraints: {}", cs.num_constraints());
         assert!(cs.is_satisfied().unwrap());
     }
+
+
+    #[test]
+    fn test_voting_setup() {
+        let test_circuit = make_mocking_circuit();
+        voting_setup(test_circuit);
+    }
+
+    #[test]
+    fn test_voting_prove() {
+        use crate::circuits::voting::key_utils::{load_pk, load_vk};
+        let test_circuit = make_mocking_circuit();
+        println!("loading ...");
+        let pk = load_pk();
+        let vk = load_vk();
+        println!("loaded !");
+        println!("vk.alpha_g1: {:?}\n", vk.alpha_g1);
+        println!("vk.beta_g2: {:?}\n", vk.beta_g2);
+        println!("vk.gamma_g2: {:?}\n", vk.gamma_g2);
+        println!("vk.delta_g2: {:?}\n", vk.delta_g2);
+        println!("vk.gamma_abc_g1: {:?}\n", vk.gamma_abc_g1);
+    
+        let pvk = Groth16::<Bn254>::process_vk(&vk).unwrap();
+        let mut image: Vec<_> = vec![];
+
+        image.append(&mut vec![
+            test_circuit.instance.voting_round.clone().unwrap(),
+            test_circuit.instance.root.clone().unwrap(),
+        ]);
+
+        for i in test_circuit.instance.vote_cm.clone().unwrap() {
+            image.push(*i.clone().x().unwrap());
+            image.push(*i.clone().y().unwrap());
+        }
+        
+        let mut image_for_sc = vec![];
+        for i in image.clone().iter() {
+            image_for_sc.push(i.to_string());
+        }
+
+        println!("image_for_sc: {:?}", image_for_sc);
+        println!("len(image_for_sc): {:?}", image_for_sc.len());
+        
+        let proof = voting_prove(pk, test_circuit);
+        println!("proof: {:?}", proof);
+        assert!(Groth16::<Bn254>::verify_with_processed_vk(&pvk, &image, &proof).unwrap());
+    }   
 }
